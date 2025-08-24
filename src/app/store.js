0bc9c891963d507184c3367bbb002151cf5cce6c
haosun68 <haosun8686@gmail.com>
2025-08-24 16:41:18 -0500

    setup the Store

---
+++ src/app/store.js
@@ -0,0 +1,31 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import createFilter from "redux-persist-transform-filter";
+import userSlice from "../features/userSlice";
+
+//saveUserOnlyFilter
+const saveUserOnlyFilter = createFilter("user", ["user.user"]);
+
+//persist config
+const persistConfig = {
+  key: "user",
+  storage,
+  whitelist: ["user"],
+  transforms: [saveUserOnlyFilter],
+};
+
+const rootReducer = combineReducers({ user: userSlice });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+  devTools: true,
+});
+
+export const persistor = persistStore(store);
