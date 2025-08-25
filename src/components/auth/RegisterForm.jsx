696b779f77848c9ccecd83b516d445df21ab0627
haosun68 <haosun8686@gmail.com>
2025-08-25 13:17:11 -0500

    Implement register/login pages with react-hook-form + yup validation and custom inputs

---
+++ src/components/auth/RegisterForm.jsx
@@ -0,0 +1,63 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { signUpSchema } from "../../utils/validation";
+import AuthInput from "./AuthInput";
+
+export default function RegisterForm() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(signUpSchema),
+    mode: "onChange",
+  });
+  const onSubmit = (data) => console.log(data);
+  console.log("values", watch());
+  console.log("errors", errors);
+  return (
+    <div className="h-screen w-full flex items-center justify-center overflow-hidden">
+      {/* Container */}
+      <div className="max-w-md space-y-8 p-10 dark:bg-dark_bg_2 rounded-xl">
+        {/*Heading*/}
+        <div className="text-center dark:text-dark_text_1">
+          <h2 className="mt-6 text-3xl font-bold">Welcome</h2>
+          <p className="mt-2 text-sm">Sign up</p>
+        </div>
+        {/*Form*/}
+        <form onSubmit={handleSubmit(onSubmit)} className="mt-6 space-y-6">
+          <AuthInput
+            name="name"
+            type="text"
+            placeholder="Full Name"
+            register={register}
+            error={errors?.name?.message}
+          />
+          <AuthInput
+            name="email"
+            type="email"
+            placeholder="Email"
+            register={register}
+            error={errors?.email?.message}
+          />
+          <AuthInput
+            name="status"
+            type="text"
+            placeholder="Status"
+            register={register}
+            error={errors?.status?.message}
+          />
+          <AuthInput
+            name="password"
+            type="password"
+            placeholder="Password"
+            register={register}
+            error={errors?.password?.message}
+          />
+          <button type="submit">submit</button>
+        </form>
+      </div>
+    </div>
+  );
+}
