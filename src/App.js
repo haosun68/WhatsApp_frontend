08b440e19000998438a1ad1259791a3e5885399b
haosun68 <haosun8686@gmail.com>
2025-08-25 20:42:44 -0500

    complete signup form UI and implement register action with AsyncThunk

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Register from "./pages/register";
-
 function App() {
+  const { user } = useSelector((state) => ({ ...state }));
   return (
     <div className="dark">
       <Router>
--- src/components/auth/RegisterForm.jsx
+++ src/components/auth/RegisterForm.jsx
@@ -2,8 +2,14 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { signUpSchema } from "../../utils/validation";
 import AuthInput from "./AuthInput";
-
+import { useDispatch, useSelector } from "react-redux";
+import PulseLoader from "react-spinners/PulseLoader";
+import { Link, useNavigate } from "react-router-dom";
+import { registerUser } from "../../features/userSlice";
 export default function RegisterForm() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { status, error } = useSelector((state) => state.user);
   const {
     register,
     handleSubmit,
@@ -11,11 +17,13 @@ export default function RegisterForm() {
     formState: { errors },
   } = useForm({
     resolver: yupResolver(signUpSchema),
-    mode: "onChange",
   });
-  const onSubmit = (data) => console.log(data);
-  console.log("values", watch());
-  console.log("errors", errors);
+  const onSubmit = async (data) => {
+    let res = await dispatch(registerUser({ ...data, picture: "" }));
+    if (res.payload.user) {
+      navigate("/");
+    }
+  };
   return (
     <div className="h-screen w-full flex items-center justify-center overflow-hidden">
       {/* Container */}
@@ -36,8 +44,8 @@ export default function RegisterForm() {
           />
           <AuthInput
             name="email"
-            type="email"
-            placeholder="Email"
+            type="text"
+            placeholder="Email address"
             register={register}
             error={errors?.email?.message}
           />
@@ -55,7 +63,35 @@ export default function RegisterForm() {
             register={register}
             error={errors?.password?.message}
           />
-          <button type="submit">submit</button>
+          {/*if we have an error*/}
+          {error ? (
+            <div>
+              <p className="text-red-400">{error}</p>
+            </div>
+          ) : null}
+          {/*Submit button*/}
+          <button
+            className="w-full flex justify-center bg-green_1 text-gray-100 p-4 rounded-full tracking-wide
+          font-semibold focus:outline-none hover:bg-green_2 shadow-lg cursor-pointer transition ease-in duration-300
+          "
+            type="submit"
+          >
+            {status === "loading" ? (
+              <PulseLoader color="#fff" size={16} />
+            ) : (
+              "Sign up"
+            )}
+          </button>
+          {/* Sign in link */}
+          <p className="flex flex-col items-center justify-center mt-10 text-center text-md dark:text-dark_text_1">
+            <span>have an account ?</span>
+            <Link
+              href="/login"
+              className=" hover:underline cursor-pointer transition ease-in duration-300"
+            >
+              Sign in
+            </Link>
+          </p>
         </form>
       </div>
     </div>
