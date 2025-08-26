ce1e5cb6d294ab9576a46dccdef35fea7c7781fc
haosun68 <haosun8686@gmail.com>
2025-08-26 15:14:38 -0500

    add login form

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -12,5 +12,15 @@ export const signUpSchema = Yup.object({
   status: Yup.string().max(64, "Status must be less than 64 characters."),
   password: Yup.string()
     .required("Password is required.")
-    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/, "Password must contain atleast 6 characters, 1 uppercase, 1 lowercase, 1 number and 1 special character."),
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
+      "Password must contain atleast 6 characters, 1 uppercase, 1 lowercase, 1 number and 1 special character."
+    ),
+});
+
+export const signInSchema = Yup.object({
+  email: Yup.string()
+    .required("Email address is required.")
+    .email("Invalid email address."),
+  password: Yup.string().required("Password is required."),
 });
